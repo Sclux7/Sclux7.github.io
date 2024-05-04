e84b4dbf0376da7ea475c901a3c3c7e0cbf30932
Sclux7 <167189374+Sclux7@users.noreply.github.com>
2024-05-04 13:28:18 +1000

    j

--- listthing.js
+++ listthing.js
@@ -1,12 +1,13 @@
-var wlist = ['Hybrid theory', 'Meteora', 'Fallen', 'Yankee Hotel Foxtrot', 'Toxicity', 'Follow the Leader', 'Issues', 'Half-Life', 'Memento', 'GoldSrc', 'Quake'];
+var wlist = ['Hybrid Theory', 'Meteora', 'Fallen', 'Yankee Hotel Foxtrot', 'Toxicity', 'Follow the Leader', 'Issues', 'Half-Life', 'Memento', 'GoldSrc', 'Quake'];
 var current = 0;
+var element = $('#target');
 
 setInterval(function(){
+    (current == wlist.length) ? 0 : current;
     var x = list(current);
     $('p').each(function(){
-        $this.find('strong').fadeOut('fast', function(){
+        $this.find(element).fadeOut('fast', function(){
             $(this).html(x).fadeIn();
         });
     });
-    (current == wlist.length) ? 0 : current++;
 }, 3000);
