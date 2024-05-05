5058321d62fa69f10338f0b53b25ee63cdcacb50
Sclux7 <167189374+Sclux7@users.noreply.github.com>
2024-05-05 19:03:26 +1000

    q

---
+++ rpsdeluxe.js
@@ -0,0 +1,41 @@
+var options = ['rock','paper','scissors'];
+var selected = 0;
+var element = $('#target2');
+
+function rpsselection(){
+    var choice = Math.floor(Math.random() * (options.length));
+    document.getElementById("rpsDisplay").innerHTML = facts(choice);
+    if(rps1() == true){
+        if(choice == 0){
+            document.getElementById("rpsresult").innerHTML = "draw";
+        }
+        if(choice == 1){
+            document.getElementById("rpsresult").innerHTML = "You Lose :(";
+        }
+        if(choice == 2){
+            document.getElementById("rpsresult").innerHTML = "You Win :D";
+        }
+    }
+    if(rps2() == true){
+        if(choice == 1){
+            document.getElementById("rpsresult").innerHTML = "draw";
+        }
+        if(choice == 2){
+            document.getElementById("rpsresult").innerHTML = "You Lose :(";
+        }
+        if(choice == 0){
+            document.getElementById("rpsresult").innerHTML = "You Win :D";
+        }
+    }
+    if(rps3() == true){
+        if(choice == 2){
+            document.getElementById("rpsresult").innerHTML = "draw";
+        }
+        if(choice == 0){
+            document.getElementById("rpsresult").innerHTML = "You Lose :(";
+        }
+        if(choice == 1){
+            document.getElementById("rpsresult").innerHTML = "You Win :D";
+        }
+    }
+}
