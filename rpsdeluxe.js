e404200cb55c5feb328eb435772ed89497c717e7
Sclux7 <167189374+Sclux7@users.noreply.github.com>
2024-05-05 20:11:32 +1000

    Update rpsdeluxe.js

--- rpsdeluxe.js
+++ rpsdeluxe.js
@@ -10,35 +10,35 @@ function rpsselection(playerSelection){
     document.getElementById("rpsDisplay").innerHTML = options(selected);
     // elementt.html(options[selected]);
     if (playerSelection === 1){
-        if(options = 0){
+        if(options[0]){
             document.getElementById("rpsresult").innerHTML = "draw";
         }
-        if(options = 1){
+        if(options[1]){
             document.getElementById("rpsresult").innerHTML = "You Lose :(";
         }
-        if(options = 2){
+        if(options[2]){
             document.getElementById("rpsresult").innerHTML = "You Win :D";
         }
     }
     else if (playerSelection === 2){
-        if(options == 1){
+        if(options[1]){
             document.getElementById("rpsresult").innerHTML = "draw";
         }
-        if(options == 2){
+        if(options[2]){
             document.getElementById("rpsresult").innerHTML = "You Lose :(";
         }
-        if(options == 0){
+        if(options[0]){
             document.getElementById("rpsresult").innerHTML = "You Win :D";
         }
     }
     else if (playerSelection === 3){
-        if(options == 2){
+        if(options[2]){
             document.getElementById("rpsresult").innerHTML = "draw";
         }
-        if(options == 0){
+        if(options[0]){
             document.getElementById("rpsresult").innerHTML = "You Lose :(";
         }
-        if(options == 1){
+        if(options[1]){
             document.getElementById("rpsresult").innerHTML = "You Win :D";
         }
     }
