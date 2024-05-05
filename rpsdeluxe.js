0b70ea3c1835622ae2e212510d29ae1facc18749
Sclux7 <167189374+Sclux7@users.noreply.github.com>
2024-05-05 20:19:48 +1000

    Update rpsdeluxe.js

--- rpsdeluxe.js
+++ rpsdeluxe.js
@@ -7,38 +7,38 @@ var rps3 = "1scissors";
 
 function rpsselection(playerSelection){
     selected = Math.floor(Math.random() * (options.length));
-    document.getElementById("rpsDisplay").innerHTML = options(selected);
+    document.getElementById("rpsDisplay").innerHTML = options[selected];
     // elementt.html(options[selected]);
     if (playerSelection === 1){
-        if(options[0]){
+        if(selected == 0){
             document.getElementById("rpsresult").innerHTML = "draw";
         }
-        if(options[1]){
+        if(selected == 1){
             document.getElementById("rpsresult").innerHTML = "You Lose :(";
         }
-        if(options[2]){
+        if(selected == 2){
             document.getElementById("rpsresult").innerHTML = "You Win :D";
         }
     }
     else if (playerSelection === 2){
-        if(options[1]){
+        if(selected == 1){
             document.getElementById("rpsresult").innerHTML = "draw";
         }
-        if(options[2]){
+        if(selected == 2){
             document.getElementById("rpsresult").innerHTML = "You Lose :(";
         }
-        if(options[0]){
+        if(selected == 0){
             document.getElementById("rpsresult").innerHTML = "You Win :D";
         }
     }
     else if (playerSelection === 3){
-        if(options[2]){
+        if(selected == 2){
             document.getElementById("rpsresult").innerHTML = "draw";
         }
-        if(options[0]){
+        if(selected == 0){
             document.getElementById("rpsresult").innerHTML = "You Lose :(";
         }
-        if(options[1]){
+        if(selected == 1){
             document.getElementById("rpsresult").innerHTML = "You Win :D";
         }
     }
