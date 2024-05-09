a9fca782872da4c0710e659a2deb413cae755d35
Sclux7 <167189374+Sclux7@users.noreply.github.com>
2024-05-09 13:20:22 +1000

    8

--- popupbox.js
+++ popupbox.js
@@ -1,9 +1,11 @@
 /*this is garbage that i mostly copied i will definitely fix this at some point i promise i would
  never ever ever ever ever ever ever ever ever ever ever ever leave a coding project half finished*/
-dragable(document.getElementById("pubox"));
+dragableElement(document.getElementById("pubox"));
+var currentElement;
 
-function dragable(elemlent){
+function dragableElement(elemlent){
     var p1 = 0, p2 = 0, p3 = 0, p4 = 0;
+    currentElement = elemlent;
     if (document.getElementById(elemlent.id + "header")) {
         document.getElementById(elemlent.id + "header").onmousedown = dragablemdown;
     } else {
@@ -12,18 +14,23 @@ function dragable(elemlent){
 }
 
 function dragablemdown(z){
+    z = z || window.Event;
+    z.preventDefault();
     p3 = z.clientX;
     p4 = z.clientY;
+    document.onmouseup = dragablestop;
     document.onmousemove = dragablemove;
 }
 
 function dragablemove(z){
+    z = z || window.Event;
+    z.preventDefault();
     p1 = p3 - z.clientX;
     p2 = p4 - z.clientY;
     p3 = z.clientX;
     p4 = z.clientY;
-    elemlent.style.top = (elemlent.offsetTop - p2) + "px";
-    elemlent.style.left = (elemlent.offsetLeft - p1) + "px";
+    currentElement.style.top = (currentElement.offsetTop - p2) + "px";
+    currentElement.style.left = (currentElement.offsetLeft - p1) + "px";
 }
 
 function dragablestop(){
