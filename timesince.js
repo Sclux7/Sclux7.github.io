7849486dbb34f8a9389a35bc8752f9d2f4cff109
Sclux7 <40minstoxen@gmail.com>
2024-09-24 17:45:44 +1000

    fixed

--- timesince.js
+++ timesince.js
@@ -4,20 +4,23 @@ const targetSecondsMilestone = [12422400, 13000000, 14000000, 15000000];  // new
 function findNewTarget(index){
     const newTargetM = targetSecondsMilestone[index] + 1000000;
     targetSecondsMilestone[index] = newTargetM;
+    console.log(`Target ${index} updated to ${newTargetM}`);
 }
 
 function counterSeconds(){
     const now = new Date();
     const differenceSeconds = Math.floor((now - originalDate) / 1000);
     const counter = document.getElementById('counter');
-    counter.textContent = `${differenceSeconds}`
+    counter.textContent = `${differenceSeconds}` ;
 
     targetSecondsMilestone.forEach((targetSeconds, index) => {
         const remainingSeconds = targetSeconds - differenceSeconds;  // new
-        const countdown = document.getElementById('countdown-${index}');  // new
-        countdown.textContent = remainingSeconds >= 0 ? `${remainingSeconds}` : "0";  // new
+        const countdown = document.getElementById(`countdown-${index}`);  // new
+        if (countdown) {
+            countdown.textContent = remainingSeconds >= 0 ? `${remainingSeconds}` : "0";  // new
+        }
         if (remainingSeconds <= 0) {
-            findNewTarget(index)
+            findNewTarget(index);
         }
     });
 
