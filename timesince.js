70cd97b75336e7060b14d52597e64a7d8e5e60f4
Sclux7 <40minstoxen@gmail.com>
2024-09-24 17:06:17 +1000

    new stuff!!! countdown

--- timesince.js
+++ timesince.js
@@ -1,9 +1,26 @@
 const originalDate = new Date('2024-05-03T12:29:30Z');
+const targetSecondsMilestone = [12422400, 13000000, 14000000, 15000000];  // new
+
+function findNewTarget(index){
+    const newTargetM = targetSecondsMilestone[index] + 1000000;
+    targetSecondsMilestone[index] = newTargetM;
+}
+
 function counterSeconds(){
     const now = new Date();
     const differenceSeconds = Math.floor((now - originalDate) / 1000);
     const counter = document.getElementById('counter');
     counter.textContent = `${differenceSeconds}`
+
+    targetSecondsMilestone.forEach((targetSeconds, index) => {
+        const remainingSeconds = targetSeconds - differenceSeconds;  // new
+        const countdown = document.getElementById('countdown-${index}');  // new
+        countdown.textContent = remainingSeconds >= 0 ? `${remainingSeconds}` : "0";  // new
+        if (remainingSeconds <= 0) {
+            findNewTarget(index)
+        }
+    });
+
     setTimeout(counterSeconds, 1000);
 };
 
