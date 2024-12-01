c823e219582ce30b6d17542517bc48837c2b6fb7
Sclux7 <40minstoxen@gmail.com>
2024-12-02 10:29:44 +1100

    important notice

--- timesince.js
+++ timesince.js
@@ -1,8 +1,9 @@
 const originalDate = new Date('2024-05-03T12:29:30Z');
+const endOfYearDate = new Date('2025-01-01T00:00:00Z')
 const targetSecondsMilestone = [13000000, 14000000, 15000000, 16000000];  // new
 
 function findNewTarget(index){
-    const newTargetM = targetSecondsMilestone[index] + 1000000;
+    const newTargetM = targetSecondsMilestone[index] + 1000000;  //this doesnt work atm, fix?
     targetSecondsMilestone[index] = newTargetM;
     console.log(`Target ${index} updated to ${newTargetM}`);
 }
@@ -11,7 +12,11 @@ function counterSeconds(){
     const now = new Date();
     const differenceSeconds = Math.floor((now - originalDate) / 1000);
     const counter = document.getElementById('counter');
-    counter.textContent = `${differenceSeconds}` ;
+    counter.textContent = `${differenceSeconds}`;
+
+    const secondsLeft = Math.floor((endOfYearDate - now) / 1000);
+    const countdowneoy = document.getElementById('countdowneoy');
+    countdowneoy.textContent = `${secondsLeft}`;
 
     targetSecondsMilestone.forEach((targetSeconds, index) => {
         const remainingSeconds = targetSeconds - differenceSeconds;  // new
